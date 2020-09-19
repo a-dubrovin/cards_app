21d73929dbf689e51e7600dd091ab4d77bad18a8
Anton Dubrovin <s000561@gmail.com>
2020-09-19 16:17:37 +0300

    Add allowed host for CORS

--- backend/card_app/settings.py
+++ backend/card_app/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
 
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -134,6 +136,10 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     )
 }
+
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:8080',
+]
 
 # Cards app settings
 SERIES_LIMIT = (1, 9999)
